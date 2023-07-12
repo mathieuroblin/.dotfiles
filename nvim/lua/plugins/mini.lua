09bdc8f3546fa720cc5d1471ede531fe65ad1716
Mathieu Roblin <mathieu@roblin-it.com>
2023-07-13 00:39:53 +0800

    Add some mini plugins and ref to potential future ones

---
+++ nvim/lua/plugins/mini.lua
@@ -0,0 +1,49 @@
+--[[
+--  Multi plugins from: https://github.com/echasnovski/mini.nvim.
+--]]
+return {
+  {
+    -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-align.md
+    --[[
+    {
+      'echasnovski/mini.align',
+      version = '*',
+      config = function ()
+        require('mini.align').setup({
+          mappings = {
+            start = '<leader>ga',
+            start_with_preview = '<leader>gA',
+          },
+        })
+      end,
+    },
+    --]]
+
+    -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-jump.md
+    --[[
+    {
+      'echasnovski/mini.jump',
+      version = '*',
+      config = true,
+    },
+    --]]
+
+    -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-pairs.md
+    {
+      'echasnovski/mini.pairs',
+      version = '*',
+      config = true,
+    },
+
+    -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-splitjoin.md
+    --[[
+    {
+      'echasnovski/mini.splitjoin',
+      version = '*',
+    },
+    --]]
+
+    -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-surround.md
+    -- { 'echasnovski/mini.surround', version = '*' },
+  }
+}
