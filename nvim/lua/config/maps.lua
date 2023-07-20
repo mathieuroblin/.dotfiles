fcf3747d36e51be639c9ada071e11ba812245041
Mathieu Roblin <mathieu@roblin-it.com>
2023-07-21 00:33:19 +0800

    Add mappings to handle common buffer ops

--- nvim/lua/config/maps.lua
+++ nvim/lua/config/maps.lua
@@ -1,1 +1,7 @@
+-- General Mappings
 vim.keymap.set({ 'n', 'v' }, '<leader>n', '<cmd>nohlsearch<cr>', { silent = true, desc = 'Turn off search highlighting' })
+
+-- Buffer Management
+vim.keymap.set({ 'n' }, '<leader>bd', '<cmd>bdelete<cr>', { silent = true, desc = 'Close buffer' })
+vim.keymap.set({ 'n' }, '<leader>bn', '<cmd>bnext<cr>', { silent = true, desc = 'Next buffer' })
+vim.keymap.set({ 'n' }, '<leader>bp', '<cmd>bprevious<cr>', { silent = true, desc = 'Previous buffer' })
