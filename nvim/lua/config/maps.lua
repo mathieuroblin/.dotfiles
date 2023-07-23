1c3bc040af05f90106aee1a4569037a33c4547d9
Mathieu Roblin <mathieu@roblin-it.com>
2023-07-23 11:18:33 +0800

    Add base mappings for window management

--- nvim/lua/config/maps.lua
+++ nvim/lua/config/maps.lua
@@ -5,3 +5,8 @@ vim.keymap.set({ 'n', 'v' }, '<leader>n', '<cmd>nohlsearch<cr>', { silent = true
 vim.keymap.set({ 'n' }, '<leader>bd', '<cmd>bdelete<cr>', { silent = true, desc = 'Close buffer' })
 vim.keymap.set({ 'n' }, '<leader>bn', '<cmd>bnext<cr>', { silent = true, desc = 'Next buffer' })
 vim.keymap.set({ 'n' }, '<leader>bp', '<cmd>bprevious<cr>', { silent = true, desc = 'Previous buffer' })
+
+vim.keymap.set({ 'n' }, '<leader>ws', '<cmd>split<cr>', { silent = true, desc = 'Horizontal split' })
+vim.keymap.set({ 'n' }, '<leader>wv', '<cmd>vsplit<cr>', { silent = true, desc = 'Vertical split' })
+vim.keymap.set({ 'n' }, '<leader>wq', '<cmd>quit<cr>', { silent = true, desc = 'Close' })
+vim.keymap.set({ 'n' }, '<leader>wo', '<cmd>only<cr>', { silent = true, desc = 'Close others' })
