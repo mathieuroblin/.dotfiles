21421d665165e75e8b02be5f4af358fa4301b1b8
Mathieu Roblin <mathieu@roblin-it.com>
2023-07-27 12:21:12 +0800

    Add tabline config

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -77,6 +77,40 @@ return {
         options = {
           theme = 'catppuccin',
         },
+        sections = {
+          lualine_a = { 'mode' },
+          lualine_b = {
+            -- 'branch',
+            'diff',
+            'diagnostics'
+          },
+          lualine_c = {
+            {
+              'filename',
+              file_status = true,
+              path = 1,
+              symbols = {
+                readonly = '[R]'
+              },
+            },
+          },
+          lualine_x = {
+            -- 'encoding',
+            -- 'fileformat',
+            'filetype'
+          },
+          lualine_y = { 'progress' },
+          lualine_z = { 'location' }
+        },
+        tabline = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = { 'tabs' }
+        },
+        extensions = { 'fugitive' },
       })
     end
     --[[
