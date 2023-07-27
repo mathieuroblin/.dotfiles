a4a4aa8f24e5a4a60105069e2e4d2a3cd08e2fea
Mathieu Roblin <mathieu@roblin-it.com>
2023-07-27 12:20:57 +0800

    Add quick change window

--- nvim/lua/config/maps.lua
+++ nvim/lua/config/maps.lua
@@ -6,6 +6,7 @@ vim.keymap.set({ 'n' }, '<leader>bd', '<cmd>bdelete<cr>', { silent = true, desc 
 vim.keymap.set({ 'n' }, '<leader>bn', '<cmd>bnext<cr>', { silent = true, desc = 'Next buffer' })
 vim.keymap.set({ 'n' }, '<leader>bp', '<cmd>bprevious<cr>', { silent = true, desc = 'Previous buffer' })
 
+vim.keymap.set({ 'n' }, '<leader>ww', '<c-w><c-w>', { silent = true, desc = 'Change Window' })
 vim.keymap.set({ 'n' }, '<leader>ws', '<cmd>split<cr>', { silent = true, desc = 'Horizontal split' })
 vim.keymap.set({ 'n' }, '<leader>wv', '<cmd>vsplit<cr>', { silent = true, desc = 'Vertical split' })
 vim.keymap.set({ 'n' }, '<leader>wq', '<cmd>quit<cr>', { silent = true, desc = 'Close' })
